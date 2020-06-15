0bcfac63eed52f66e1a635d90a41ded1c96ca13d
github username <constance.hyman@mchsi.com>
2020-06-14 20:56:54 -0700

    Add edit and delete functionality to script.js

--- script.js
+++ script.js
@@ -33,3 +33,48 @@
 			addInput.value = '';
 		}	
 	})
+
+
+
+			//******************Edit and Delete Notes
+
+			ul.addEventListener('click', function(e){
+				// e.preventDefault();
+
+				if(e.target.classList[1] === "fa-pencil-square-o") {
+					
+					var parentPar = e.target.parentNode;
+					parentPar.style.display = 'none';
+
+					var note = parentPar.previousElementSibling;
+					var input = parentPar.nextElementSibling;
+
+					input.style.display = 'block';
+
+					input.value = note.textContent;
+
+					input.addEventListener('keypress', function(e){
+						if(e.keyCode === 13) {
+							if(input.value !== ''){
+							note.textContent = input.value;
+							parentPar.style.display = 'block';
+							input.style.display = 'none';
+
+						} else {
+
+								var li = input.parentNode;
+								li.parentNode.removeChild(li);
+						}
+
+						} 
+
+					});
+
+				} else if (e.target.classList[1] === "fa-times") {
+						var list = e.target.parentNode.parentNode;
+						list.parentNode.removeChild(list);
+						console.log(e.target.classList);
+				}
+
+				
+			})
